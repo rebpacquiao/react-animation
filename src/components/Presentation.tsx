e7622c85d27f232d25d67bd750a9960a60a32830
rebpacquiao <reb.pacquiao@gmail.com>
2024-10-22 10:27:49 +0800

    fix error on next slide

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,34 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import slidesData from "../data/slides.json";
 
-const NextArrow = ({ onClick }) => (
-  <div
-    className="absolute top-1/2 right-4 transform -translate-y-1/2 text-white cursor-pointer z-10"
-    onClick={onClick}
-  >
-    ➔
-  </div>
-);
-
-const PrevArrow = ({ onClick }) => (
-  <div
-    className="absolute top-1/2 left-4 transform -translate-y-1/2 text-white cursor-pointer z-10"
-    onClick={onClick}
-  >
-    ←
-  </div>
-);
-
 const Presentation = () => {
+  const sliderRef = useRef<Slider | null>(null);
+
   const settings = {
     dots: true,
     infinite: true,
+    lazyLoad: true,
     fade: true,
-    arrows: true,
-    speed: 300,
+    speed: 1000,
     slidesToShow: 1,
+    swipeToSlide: false,
     slidesToScroll: 1,
+  };
+
+  const handleNextSlide = () => {
+    console.log("test");
+    if (sliderRef.current) {
+      sliderRef.current.slickNext();
+    }
   };
 
   return (
@@ -39,7 +31,7 @@ const Presentation = () => {
           <a href="#">Discover More</a>
         </div>
       </div>
-      <Slider {...settings}>
+      <Slider ref={sliderRef} {...settings}>
         {slidesData.map((slide) => (
           <div key={slide.id} className="slide relative">
             {slide.type === "video" && (
@@ -49,7 +41,7 @@ const Presentation = () => {
                   autoPlay
                   loop
                   muted
-                  className="w-full h-full object-cover"
+                  className="w-full object-cover"
                 />
                 <div className="overlay absolute inset-0 flex flex-col items-center justify-center bg-[#000] bg-opacity-50 transition-opacity duration-500">
                   <h3 className="text-white text-3xl md:text-4xl font-bold">
@@ -58,6 +50,21 @@ const Presentation = () => {
                   <p className="text-white text-lg md:text-xl mt-2">
                     {slide.content.description}
                   </p>
+                </div>
+                <div className="arrow-down">
+                  <button onClick={handleNextSlide}>
+                    <svg
+                      width="80px"
+                      height="80px"
+                      viewBox="0 0 48 48"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M0 0h48v48H0z" fill="none" />
+                      <g id="Shopicon">
+                        <polygon points="24,29.172 9.414,14.586 6.586,17.414 24,34.828 41.414,17.414 38.586,14.586 	" />
+                      </g>
+                    </svg>
+                  </button>
                 </div>
               </>
             )}
